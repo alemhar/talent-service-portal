8d22feaef57e2c4893c7daf155b6fe623f074015
Alemhar Salihuddin <alemhar@gmail.com>
2020-06-28 23:07:11 +0800

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+require('./bootstrap');
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+import {routes} from './routes'; 
+//window.Vue = require('vue');
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const router = new VueRouter({
+    routes,
+    mode: 'history'
+})
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
