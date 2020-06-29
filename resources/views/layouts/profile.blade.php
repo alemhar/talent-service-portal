86a495e6f44af9bbc81572f9002a15870e1c45d1
Alemhar Salihuddin <alemhar@gmail.com>
2020-06-29 20:08:16 +0800

    Added Basic Info Page

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -81,13 +81,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
           <li class="nav-item">
-            <a href="/basic-info" class="nav-link">
+            <router-link to="/basic-info" class="nav-link">
+           
               <i class="nav-icon fas fa-id-card"></i>
               <p>
                 Basic Information
                 <!-- span class="right badge badge-danger">New</span -->
               </p>
-            </a>
+            </router-link>
           </li>
           <li class="nav-item">
             <a href="#" class="nav-link">
