85fbc731c909fe2ff76e146a6d6d0d45180dc1b6
Alemhar Salihuddin <alemhar@gmail.com>
2020-06-29 20:18:41 +0800

    Added Basic Info Page

--- resources/js/components/BasicInfo.vue
+++ resources/js/components/BasicInfo.vue
@@ -4,7 +4,6 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">Basic Info</div>
-
                     <div class="card-body">
                         BASIC INFO
                     </div>
